92d3e3c7ddec624122e2b279797160870a36be2d
Tabindanoor <tabindanoor415@gmail.com>
2025-12-25 23:37:39 +0500

    enjoy brother enjoy

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,6 @@
-import Link from "next/link";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import data from "../data/data.json";
-import Image from "next/image";
-
-type EventCategory = {
-  id: string;
-  title: string;
-  description: string;
-  image: string;
-};
-
+import HomePage from "../components/homepage/HomePage";
 
 export default function Home() {
-  const { events_categories } = data as {
-    events_categories: EventCategory[];
-  };
-  // console.log(events_categories);
-  return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
-      <main className="flex-1">
-      
-        {events_categories.map((cat) => (
-          <div key={cat.id} id={cat.id}>
-            <Link href={`/events/${cat.id}`}>
-            <h2 className="text-xl font-semibold text-gray-900 dark:text-white">
-              {cat.title}
-            </h2>
-            <Image src={cat.image} alt={cat.title} width={200} height={200}/>
-            <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">
-              {cat.description}
-            </p>
-            </Link>
-          </div>
-        ))}
-      </main>
-      <Footer />
-    </div>
-  );
+  return <HomePage data={data} />;
 }
--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,4 +1,25 @@
+import EventsPage from "../events/EventsPage";
 
-export default function HomePage(){
+type EventCategory = {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+};
 
+type Props = {
+  data: {
+    events_categories: EventCategory[];
+  };
+};
+
+export default function HomePage({ data }: Props) {
+  return (
+    <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
+      <h1 className="mb-8 text-3xl font-bold text-gray-900 dark:text-white">
+        Events
+      </h1>
+      <EventsPage data={data} />
+    </div>
+  );
 }
