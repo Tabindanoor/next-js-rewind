89f2d58f673440a302fe3ed3dde277ebfe50e8f8
Tabindanoor <tabindanoor415@gmail.com>
2025-12-27 20:19:28 +0500

    enjoy brother enjoy

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  return NextResponse.json({ name: "Tabinda Nooor with a new enthusiasm" });
+  return NextResponse.json({ name: "Tabinda with a new enthusiasm" });
 }
 
