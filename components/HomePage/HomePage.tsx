ed7bc1fb82ec8f8809e6541eefd570230bffd8bc
Tabindanoor <tabindanoor415@gmail.com>
2026-01-10 23:58:07 +0500

    enjoy brother enjoy

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -16,9 +16,9 @@ type Props = {
 export default function HomePage({ data }: Props) {
   return (
     <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
-      <h1 className="mb-8 text-3xl font-bold text-gray-900 dark:text-white">
+      {/* <h1 className="mb-8 text-3xl font-bold text-gray-900 dark:text-white">
         Events
-      </h1>
+      </h1> */}
       <EventsPage data={data} />
     </div>
   );
