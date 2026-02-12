dc7b53bb2df5949f04393061db0fba44bec916f4
Tabindanoor <tabindanoor415@gmail.com>
2026-02-12 22:45:03 +0500

    learning

--- components/events/EventStats.tsx
+++ components/events/EventStats.tsx
@@ -19,9 +19,9 @@ export default function EventStats({
           </p>
         </div>
         <div className="text-right">
-          <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+          {/* <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
             Event ID
-          </p>
+          </p> */}
           <p className="mt-1 font-mono text-sm text-gray-600 dark:text-gray-400">
             {eventId}
           </p>
